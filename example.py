0b02efe65c7d7a456ef3b887a6d6409cb4ffe2e4
gepu <gepu@bitsim.com>
2019-02-26 10:55:59 +0100

    Visualise various threshold levels

--- example.py
+++ example.py
@@ -3,22 +3,22 @@ import otsu
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def main():
-    L = 256 # number of levels
+    L = 256  # number of levels
     M = 32  # number of bins for bin-grouping normalisation
 
     N = L // M
-
     # Load original image
-    img = cv2.imread('data/soil.jpg', 0) # read image in as grayscale
-    plt.figure()
-    plt.imshow(img, cmap='gray')
+    img = cv2.imread('data/soil.jpg', 0)  # read image in as grayscale
+    # plt.figure()
+    # plt.imshow(img, cmap='gray')
 
     # Blur image
-    img = cv2.GaussianBlur(img, (7,7), 0)
-    img = cv2.GaussianBlur(img, (7,7), 0)
-    plt.figure()
-    plt.imshow(img, cmap='gray')
+    img = cv2.GaussianBlur(img, (7, 7), 0)
+    img = cv2.GaussianBlur(img, (7, 7), 0)
+    # plt.figure()
+    # plt.imshow(img, cmap='gray')
 
     # Calculate histogram
     hist = cv2.calcHist(
@@ -31,18 +31,21 @@ def main():
 
     # Normalise bin histogram
     norm_hist = otsu.normalised_histogram_binning(hist, M=M, L=L)
-    plt.figure()
-    plt.bar(range(0, norm_hist.shape[0]), norm_hist.ravel())
-    
+
     # Estimate valley regions
     valleys = otsu.find_valleys(norm_hist)
-    print(valleys)
-    thresholds = otsu.threshold_valley_regions(hist, valleys, N) 
-    print(thresholds)
+    # plot valleys
+    plt.figure()
+    plt.bar(range(0, norm_hist.shape[0]), norm_hist.ravel())
+    for v in valleys:
+        plt.axvline(x=v, color='red')
 
-    otsu_threshold = otsu.otsu_method(hist)
-    thresholds = otsu.modified_TSMO(hist, M=M, L=L)
-    print(otsu_threshold, thresholds)
+    thresholds = otsu.threshold_valley_regions(hist, valleys, N)
+
+    otsu_threshold, _ = otsu.otsu_method(hist)
+    thresholds2 = otsu.modified_TSMO(hist, M=M, L=L, maximum=4)
+    print('Otsu threshold: {}\nStep-by-step MTSMO: {}\nMTSMO: {}'.format(
+        otsu_threshold, thresholds, thresholds2))
 
     # Show histogram with thresholds
     plt.figure()
@@ -51,8 +54,39 @@ def main():
     for t in thresholds:
         plt.axvline(x=t, color='red')
 
+    # Illustrate thresholds
+    img_3 = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    img_auto = img_3.copy()
+    img_5 = img_3.copy()
+
+    def show_thresholds(src_img, dst_img, thresholds):
+        colors = [(255, 0, 0), (255, 128, 0), (255, 255, 0), (0, 128, 0), (0, 255, 128),
+                  (0, 255, 255), (0, 128, 255), (0, 0, 255), (128, 0, 255), (255, 0, 255), (255, 0, 127)]
+        for i, t in enumerate(sorted(thresholds)):
+            retval, mask = cv2.threshold(src_img, t, 255, cv2.THRESH_BINARY)
+            mask_ind = (mask > 0)
+            dst_img[mask_ind] = colors[i]
+        return dst_img
+
+    show_thresholds(img, img_auto, thresholds)
+    show_thresholds(img, img_3, thresholds[0:3])
+    show_thresholds(img, img_5, thresholds[0:5])
+
+    plt.figure()
+    ax = plt.subplot(2, 2, 1)
+    ax.set_title('Original image')
+    plt.imshow(img, cmap='gray')
+    ax = plt.subplot(2, 2, 2)
+    ax.set_title('{} levels (Automatic)'.format(len(thresholds)))
+    plt.imshow(img_auto)
+    ax = plt.subplot(2, 2, 3)
+    ax.set_title('3 levels')
+    plt.imshow(img_3)
+    ax = plt.subplot(2, 2, 4)
+    ax.set_title('5 levels')
+    plt.imshow(img_5)
     plt.show()
 
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
