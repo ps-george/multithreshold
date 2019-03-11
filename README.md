6111beb7c02b0f89e1622b3551a1b90320396d49
ps-george <>
2019-03-11 14:24:08 +0100

    Minor changes to example to better explain, and images added to readme

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Huang, D. Y., Lin, T. W., & Hu, W. C. (2011).
 Automatic multilevel thresholding based on two-stage Otsu’s method with cluster determination by valley estimation.
 International Journal of Innovative Computing, Information and Control, 7(10), 56315644.
 ```
+
+## Results
+### Select number of thresholds automatically
+![Automatic Thresholding](images/automatic_threshold.png)
+### Selecting top X threshold values from output (1, 3, auto)
+![Different Levels](images/different_levels.png)
+### Threshold values displayed on histogram, black line is value from vanilla Otsu's method
+![Thresh Values on Histogram](images/threshold_value_histogram.png)
--- example.py
+++ example.py
@@ -4,7 +4,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def main(path='data/soil.jpg'):
+def show_thresholds(src_img, dst_img, thresholds):
+    """Visualise thresholds."""
+    colors = [(255, 0, 0), (255, 128, 0), (255, 255, 0), (0, 128, 0), (0, 204, 102),
+              (51, 255, 255), (0, 128, 255), (0, 0, 255), (128, 0, 255), (255, 0, 255), (255, 0, 127)]
+    masks = otsu.multithreshold(src_img, thresholds)
+    for i, mask in enumerate(masks):
+        # for debugging masks
+        # background = np.zeros((dst_img.shape[0], dst_img.shape[1], 3))
+        # background[mask] = (255, 255, 255)
+        # plt.figure()
+        # plt.imshow(background)
+        dst_img[mask] = colors[i]
+    return dst_img
+
+
+def full_method(img, L=256, M=32):
+    """Obtain thresholds and image masks directly from image.
+
+    Abstracts away the need to handle histogram generation.
+    """
+    # Calculate histogram
+    hist = cv2.calcHist(
+        [img],
+        channels=[0],
+        mask=None,
+        histSize=[L],
+        ranges=[0, L]
+    )
+
+    thresholds = otsu.modified_TSMO(hist, M=M, L=L)
+    masks = otsu.multithreshold(img, thresholds)
+    return thresholds, masks
+
+
+def main(path='images/soil.jpg'):
     L = 256  # number of levels
     M = 32  # number of bins for bin-grouping normalisation
 
@@ -12,7 +46,7 @@ def main(path='data/soil.jpg'):
     # Load original image
     img = cv2.imread(path, 0)  # read image in as grayscale
 
-    # Blur image
+    # Blur image to denoise
     img = cv2.GaussianBlur(img, (5, 5), 0)
 
     # Calculate histogram
@@ -24,6 +58,7 @@ def main(path='data/soil.jpg'):
         ranges=[0, L]
     )
 
+    ### Do modified TSMO step by step
     # Normalise bin histogram
     norm_hist = otsu.normalised_histogram_binning(hist, M=M, L=L)
 
@@ -34,11 +69,15 @@ def main(path='data/soil.jpg'):
     plt.bar(range(0, norm_hist.shape[0]), norm_hist.ravel())
     for v in valleys:
         plt.axvline(x=v, color='red')
-
     thresholds = otsu.threshold_valley_regions(hist, valleys, N)
+    ###
 
-    otsu_threshold, _ = otsu.otsu_method(hist)
+    # modified_TSMO does all the steps above
     thresholds2 = otsu.modified_TSMO(hist, M=M, L=L)
+
+    # Threshold obtained through default otsu method.
+    otsu_threshold, _ = otsu.otsu_method(hist)
+
     print('Otsu threshold: {}\nStep-by-step MTSMO: {}\nMTSMO: {}'.format(
         otsu_threshold, thresholds, thresholds2))
 
@@ -50,21 +89,13 @@ def main(path='data/soil.jpg'):
         plt.axvline(x=t, color='red')
 
     # Illustrate thresholds
-    img_3 = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    img_auto = img_3.copy()
-    img_1 = img_3.copy()
+    img_1 = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    img_3 = img_1.copy()
+    img_auto = img_1.copy()
 
-    def show_thresholds(src_img, dst_img, thresholds):
-        colors = [(255, 0, 0), (255, 128, 0), (255, 255, 0), (0, 128, 0), (0, 204, 102),
-                  (51, 255, 255), (0, 128, 255), (0, 0, 255), (128, 0, 255), (255, 0, 255), (255, 0, 127)]
-        masks = otsu.multithreshold(src_img, thresholds)
-        for i, mask in enumerate(masks):
-            dst_img[mask] = colors[i]
-        return dst_img
-
-    show_thresholds(img, img_auto, thresholds)
-    show_thresholds(img, img_3, thresholds[0:3])
     show_thresholds(img, img_1, [thresholds[0]])
+    show_thresholds(img, img_3, thresholds[0:3])
+    show_thresholds(img, img_auto, thresholds)
 
     plt.figure()
     ax = plt.subplot(2, 2, 1)
@@ -80,7 +111,6 @@ def main(path='data/soil.jpg'):
     ax.set_title('1 level')
     plt.imshow(img_1)
     plt.show()
-
 
 if __name__ == '__main__':
     main()
